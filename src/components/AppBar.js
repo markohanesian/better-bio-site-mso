b57da7a55f52bb3a1dfe6963e4f22c6cc6445d8b
mso872 <mso872@gmail.com>
2024-03-08 06:57:08 -0800

    Import link

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -14,6 +14,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 const drawerWidth = 240;
 const navItems = ['Home', 'About','Services', 'Contact'];
@@ -35,7 +36,7 @@ function DrawerAppBar(props) {
       <List>
         {navItems.map((item) => (
           <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemButton sx={{ textAlign: 'center' }} component={Link} to={item.path}>
               <ListItemText primary={item} />
             </ListItemButton>
           </ListItem>
