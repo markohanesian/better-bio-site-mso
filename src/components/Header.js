16aa21aeb2f0b8b3eef26cc6a07ecb28f3f356b8
mso872 <mso872@gmail.com>
2024-03-28 05:29:22 -0700

    Update profile photo

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,7 +39,7 @@ export default function Header() {
 
   return (
     <div className={classes.container}>
-      <img className={classes.image} src="mark-headshot.jpg" alt="mark"></img>
+      <img className={classes.image} src="john-doe-headshot.jpg" alt="mark"></img>
       <Typography
         className={classes.heading}
         gutterBottom
